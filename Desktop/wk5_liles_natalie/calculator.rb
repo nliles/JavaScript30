f86ca103ae9abbbfb0bb9768e1d0264b855c27e2
Natalie_Liles <Natalieliles@gmail.com>
2016-09-06 13:58:17 -0400

    Add indentation to code.

--- Desktop/wk5_liles_natalie/calculator.rb
+++ Desktop/wk5_liles_natalie/calculator.rb
@@ -1,15 +1,15 @@
 def calculator(num1,operator, num2)
-if operator == '+'
-    return num1 + num2
-elsif operator == '-'
-    return num1 - num2
-elsif operator == '*'
-    return num1 * num2
-elsif operator == '/'
-    return num1 / num2
-else 
-	return "invalid entry"
-end
+	if operator == '+'
+    	return num1 + num2
+	elsif operator == '-'
+    	return num1 - num2
+	elsif operator == '*'
+   		return num1 * num2
+	elsif operator == '/'
+       return num1 / num2
+	else 
+	   return "invalid entry"
+	end
 end
 
 # puts calculator(5,'+',4)
@@ -20,18 +20,26 @@ end
 puts "Would you like to perform a calculation? (Answer yes or type 'quit')"
 answer = gets.chomp
 array = []
+
 until answer == "quit"
+
     puts "Please enter a number:"
     num1 = gets.to_i 
+
     puts "Select: '+' to add, '-' to subract, '/' to divide, '*' to multiply:"
     operator = gets.chomp 
+
     puts "Please enter another number:"
     num2 = gets.to_i
+
     "#{calculator(num1,operator,num2)}"
     array << "#{num1} #{operator} #{num2} = #{calculator(num1,operator,num2)}"
+
     puts "Would you like to perform another calculation? (Answer yes or type 'quit')"
     answer = gets.chomp 
+
 end
+
 calculations = array.count
 puts "#{calculations} calculations performed."
 puts array
