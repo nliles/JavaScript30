0a17c4ede3b41e529dcabe1a28e4922be18644f4
Natalie_Liles <Natalieliles@gmail.com>
2017-04-29 12:20:24 -0400

    Create timer, display time functions, and custom form settings.

---
+++ 29-CountdownTimer/application.js
@@ -0,0 +1,60 @@
+let countdown
+const timeLeft = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
+
+function timer(seconds) {
+
+	clearInterval(countdown)
+
+    const now = Date.now();
+	const then = now + seconds * 1000;
+	displayEndTime(then);
+
+	countdown = setInterval(() => {
+	 const secondsRemaining = Math.round((then - Date.now()) / 1000);
+
+	 if(secondsRemaining < 0) {
+	 	clearInterval(countdown);
+
+	 	return;
+	 }
+
+	 displayTime(secondsRemaining)
+
+	}, 1000);
+
+}
+
+function displayTime(seconds) {
+	const hours = Math.floor(seconds / 3600);
+	const remainderMinutes = seconds % 3600;
+	const minutes = Math.floor(remainderMinutes/60);
+	const remainderSeconds = seconds % 60;
+	timeLeft.textContent = `${hours ? hours + ':' : ''}${minutes < 10 ? 0 : ''}${minutes}:${remainderSeconds < 10 ? 0 : ''}${remainderSeconds}`;
+}
+
+function displayEndTime(timestamp) {
+	const end = new Date(timestamp);
+	const hours = end.getHours();
+	const adjustedHours = hours > 12 ? hours - 12 : hours;
+	const mins = end.getMinutes();
+	endTime.textContent = `Timer up at ${adjustedHours}:${mins}`;
+}
+
+function startTimer() {
+	const seconds = parseInt(this.dataset.time);
+	timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+	e.preventDefault();
+	const mins = this.minutes.value;
+	timer(mins * 60);
+	this.reset();
+})
+
+
+
+
