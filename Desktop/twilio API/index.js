212af0b6a1499c5e6f5c7d6e4a6459e949a81f38
Natalie_Liles <Natalieliles@gmail.com>
2017-06-02 09:57:20 -0400

    Get basic twilio API functionality.

---
+++ Desktop/twilio API/index.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var twilio = require('twilio');
+var bodyParser = require('body-parser');
+var io = require('socket.io')(server);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Handle an incoming request from Twilio
+app.post('/message', function(request, response) {
+    //create a TwiML response object. This object helps us generate an XML
+    //string that we will ultimately return as the result of this HTTP request
+    var twiml = new twilio.TwimlResponse();
+
+    // prepare the TwiML response
+    twiml.message(function() {
+        this.body('Text received!');
+    });
+
+    if (request.body.NumMedia > 0) {
+        io.emit("picture", request.body.MediaUrl0)
+    }
+
+    // Render an XML response
+    response.type('text/xml');
+    response.send(twiml.toString());
+});
+
+server.listen(3000, function(){
+  console.log('listening on port 3000');
+});
+
